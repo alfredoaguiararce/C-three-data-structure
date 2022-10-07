996515cb1f06a9e79961a033493d203f8f76b6f0
aaguiar-mxgs <alfredoaguiararce@gmail.com>
2022-10-07 08:35:29 -0600

    :construction: Tree data structure

---
+++ data structures/data structures/Controllers/TreeController.cs
@@ -0,0 +1,74 @@
+﻿using data_structures.DataStructures;
+using Microsoft.AspNetCore.Mvc;
+
+namespace data_structures.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TreeController : ControllerBase
+    {
+        private readonly ILogger<TreeController> logger;
+
+
+        public TreeController(ILogger<TreeController> logger)
+        {
+            this.logger = logger;
+        }
+
+        [HttpGet]
+        public int GetTreeHeight()
+        {
+
+            Tree<int> _tree = new Tree<int>();
+            _tree.Root = new TreeNode<int>() { Data = 1 };
+            _tree.Root.Children = new List<TreeNode<int>>
+            {
+             new TreeNode<int>() { Data = 50, Parent = _tree.Root },
+             new TreeNode<int>() { Data = 1, Parent = _tree.Root },
+             new TreeNode<int>() { Data = 150, Parent = _tree.Root }
+            };
+            return _tree.Root.GetHeight();
+        }
+
+
+        [HttpGet]
+        [Route("children")]
+        public List<TreeNode<int>> GetTreeChildren()
+        {
+
+            Tree<int> _tree = new Tree<int>();
+            _tree.Root = new TreeNode<int>() { Data = 1 };
+            _tree.Root.Children = new List<TreeNode<int>>
+            {
+             new TreeNode<int>() { Data = 50, Parent = _tree.Root },
+             new TreeNode<int>() { Data = 1, Parent = _tree.Root },
+             new TreeNode<int>() { Data = 150, Parent = _tree.Root }
+            };
+            return _tree.Root.Children;
+        }
+
+        [HttpGet]
+        [Route("fulltree")]
+        public Tree<int> GetTreeFull()
+        {
+
+            Tree<int> _tree = new Tree<int>();
+            _tree.Root = new TreeNode<int>() { Data = 1 };
+            _tree.Root.Children = new List<TreeNode<int>>
+            {
+             new TreeNode<int>() { Data = 50, Parent = _tree.Root },
+             new TreeNode<int>() { Data = 1, Parent = _tree.Root },
+             new TreeNode<int>() { Data = 150, Parent = _tree.Root }
+            };
+            _tree.Root.Children[1].Children = new List<TreeNode<int>>
+            {
+             new TreeNode<int>() { Data = 50, Parent = _tree.Root.Children[1] },
+             new TreeNode<int>() { Data = 1, Parent = _tree.Root.Children[1] },
+             new TreeNode<int>() { Data = 150, Parent = _tree.Root.Children[1] }
+            };
+
+            logger.LogInformation(1, "{data}", _tree);
+            return _tree;
+        }
+    }
+}
---
+++ data structures/data structures/DataStructures/TreeNode.cs
@@ -0,0 +1,25 @@
+﻿namespace data_structures.DataStructures
+{
+    public class TreeNode<T>
+    {
+        public T? Data { get; set; }
+        public TreeNode<T>? Parent { get; set; }
+        public List<TreeNode<T>>? Children { get; set; }
+        public int GetHeight()
+        {
+            int height = 1;
+            TreeNode<T> Current = this;
+            while(Current != null)
+            {
+                height++;
+                Current = Current.Parent;
+            }
+            return height;
+        }
+    }
+
+    public class Tree<T>
+    {
+        public TreeNode<T>? Root { get; set; }
+    }
+}
